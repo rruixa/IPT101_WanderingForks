278db620e13211abe5ecb5dae75f3b2e90d4b37d
MD <lampa.annerxonmeier.dancel@gmail.com>
2024-04-24 11:23:20 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IPT101_WanderingForks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
